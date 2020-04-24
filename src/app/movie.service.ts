070206022b27a82e53ad0c4e61c36d5abf0a2274
David <david.a.25897@gmail.com>
2020-04-25 04:29:16 +0530

    Service

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -7,18 +7,18 @@ import { Observable } from 'rxjs'
 export class MovieService {
   constructor(private http: HttpClient) { }
   postMovie(data): Observable<any> {
-    return this.http.post('http://movies-nodejs.herokuapp.com/create', data)
+    return this.http.post('https://movies-nodejs.herokuapp.com/create', data)
   }
   getMovies(): Observable<any> {
-    return this.http.get('http://movies-nodejs.herokuapp.com/movies')
+    return this.http.get('https://movies-nodejs.herokuapp.com/movies')
   }
   getCategory(name): Observable<any> {
-    return this.http.get('http://movies-nodejs.herokuapp.com/category/'+name)
+    return this.http.get('https://movies-nodejs.herokuapp.com/category/'+name)
   }
   updateMovie(data): Observable<any> {
-    return this.http.put('http://movies-nodejs.herokuapp.com/update',data)
+    return this.http.put('https://movies-nodejs.herokuapp.com/update',data)
   }
   deleteMovie(data): Observable<any> {
-    return this.http.delete('http://movies-nodejs.herokuapp.com/delete/'+data._id)
+    return this.http.delete('https://movies-nodejs.herokuapp.com/delete/'+data._id)
   }
 }
